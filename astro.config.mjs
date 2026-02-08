5a46c31888a1b14b248b773d577b4e5f825d0236
MeloDev <melodiaz2111@gmail.com>
2026-02-08 00:31:34 -0500

    config: update site url to support /portfolio base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://melodev2111.github.io',
+  base: '/portfolio',
   integrations: [react()],
 
   // Internationalization strategy
