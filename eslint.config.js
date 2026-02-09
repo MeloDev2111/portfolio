94a9a072dec51d1762a3c7840171e6548355701d
MeloDev <melodiaz2111@gmail.com>
2026-02-09 00:32:54 -0500

    fix: lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,10 @@
 export default [
     {
-        ignores: ["dist/**/*", ".astro/**/*", "node_modules/**/*"],
+        ignores: [
+            "dist/**/*",
+            ".astro/**/*",
+            "node_modules/**/*",
+            "CHANGELOG.md",
+        ],
     },
 ];
